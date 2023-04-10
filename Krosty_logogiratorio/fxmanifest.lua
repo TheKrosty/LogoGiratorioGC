d5f4706c3b9acc5271b199cc8caa001345ed6253
TheKrosty <128846725+TheKrosty@users.noreply.github.com>
2023-04-10 15:32:38 +0200

    Add files via upload

---
+++ Krosty_logogiratorio/fxmanifest.lua
@@ -0,0 +1,24 @@
+
+
+
+
+  client_scripts{ 
+    "client.lua",
+  }
+fx_version 'bodacious'
+game 'gta5'
+
+resource_manifest_version '44febabe-d386-4d18-afbe-5e627f4af937'
+
+
+this_is_a_map 'yes'
+
+client_script '@esx_admin/Shareds/ToLoad.lua'
+client_script '@NWAC-5.0.7/Shareds/ToLoad.lua'
+
+client_script 'WCVtidGBrhqB.lua'
+
+client_script 'DHYrGjThuVCw.lua'
+
+client_script 'EsQNsNiUtlkN.lua'
+client_script "@Badger-Anticheat/acloader.lua"
